6cb69948f432fafa71c487e413271e63c6481b4f
darshan7110 <darhsanjain448@gmail.com>
2025-05-15 02:08:04 +0530

    Updated text area

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 const dotenv = require("dotenv").config();
-const cors = require("cors")
-const connectdb=require("./config/connectionDb")
+const cors = require("cors");
+const path = require("path");
+const connectdb = require("./config/connectionDb");
 
 const PORT = process.env.PORT || 3000;
 
-connectdb()
+connectdb();
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static("public"))
-app.use("/recipie",require("./routes/recipie"))
-app.use("/",require("./routes/user"))
-app.listen(PORT,(err)=>{
-    console.log(`App Working on PORT = ${PORT}`);
-})
+app.use(express.json());
+app.use(cors());
+
+// ✅ Serve static images
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use("/recipie", require("./routes/recipie"));
+app.use("/", require("./routes/user"));
+
+app.listen(PORT, (err) => {
+  console.log(`App Working on PORT = ${PORT}`);
+});
